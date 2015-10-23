d94a2683e5f90d4dd8a698e89670debb36ae40fe
Rain <lfqyusw@gmail.com>
2015-10-23 15:46:43 +0800

    添加好友页面

---
+++ HiHome/HiHome/UiLayer/AddressPage/AddFriendFirstViewController.m
@@ -0,0 +1,61 @@
+//
+//  AddFriendFirstViewController.m
+//  HiHome
+//
+//  Created by Rain on 15/10/23.
+//  Copyright © 2015年 zykj. All rights reserved.
+//
+
+#import "AddFriendFirstViewController.h"
+#import "AddFriendSecondViewController.h"
+
+@interface AddFriendFirstViewController ()
+
+@end
+
+@implementation AddFriendFirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self initView];
+}
+
+-(void)initView{
+    UITextField *mUserNumber = [[UITextField alloc] initWithFrame:CGRectMake(8, 74, SCREEN_WIDTH - 16 - 70, 40)];
+    mUserNumber.delegate = self;
+    mUserNumber.borderStyle = UITextBorderStyleRoundedRect;
+    mUserNumber.placeholder = @"请输入用户序列号";
+    UIView *mView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+    UIImageView *search_img = [[UIImageView alloc] initWithFrame:CGRectMake(5, 2.5, 25, 25)];
+    search_img.contentMode = UIViewContentModeCenter;
+    search_img.image = [UIImage imageNamed:@"search_img"];
+    [mView addSubview:search_img];
+    mUserNumber.leftView = mView;
+    mUserNumber.leftViewMode = UITextFieldViewModeAlways;
+    [self.view addSubview:mUserNumber];
+    
+    UIButton *mSearchBtn = [[UIButton alloc] initWithFrame:CGRectMake(8 + mUserNumber.frame.size.width + 10,74, 60, 40)];
+    [mSearchBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [mSearchBtn setTitle:@"查找" forState:UIControlStateNormal];
+    mSearchBtn.backgroundColor = [UIColor colorWithRed:0.92 green:0.35 blue:0.14 alpha:1];
+    [mSearchBtn addTarget:self action:@selector(searchAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:mSearchBtn];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (void)searchAction:(id)sender {
+    AddFriendSecondViewController *addFriendSecondVC = [[AddFriendSecondViewController alloc] init];
+    addFriendSecondVC.navTitle = @"添加好友";
+    [self presentViewController:addFriendSecondVC animated:NO completion:nil];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+@end

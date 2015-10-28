9eb034c95b5195ae84f8cfec9b8883c6ae98baff
王建成 <754550398@qq.com>
2015-10-28 19:47:10 +0800

    添加忘记密码页面

--- HiHome/HiHome/UiLayer/Login/RegisterViewController.h
+++ HiHome/HiHome/UiLayer/Login/RegisterViewController.h
@@ -9,7 +9,13 @@
 #import <UIKit/UIKit.h>
 #import "BaseNavigationController.h"
 
+typedef enum _pageMode
+{
+    MODE_Reg = 0,
+    MODE_forget
+}PageMode;
+
 @interface RegisterViewController : BaseNavigationController<UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-
+@property(nonatomic) PageMode pageMode;
 @end

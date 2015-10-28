96b1485c7bb6b9ee85f6efa3c601fba97376dde2
Rain <lfqyusw@gmail.com>
2015-10-28 19:39:37 +0800

    任务提醒

---
+++ HiHome/HiHome/UiLayer/AddressPage/MarkFriendViewController.h
@@ -0,0 +1,13 @@
+//
+//  MarkFriendViewController.h
+//  HiHome
+//
+//  Created by Rain on 15/10/28.
+//  Copyright © 2015年 zykj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MarkFriendViewController : UIViewController
+
+@end
---
+++ HiHome/HiHome/UiLayer/AddressPage/MarkFriendViewController.m
@@ -0,0 +1,37 @@
+//
+//  MarkFriendViewController.m
+//  HiHome
+//
+//  Created by Rain on 15/10/28.
+//  Copyright © 2015年 zykj. All rights reserved.
+//
+
+#import "MarkFriendViewController.h"
+
+@interface MarkFriendViewController ()
+
+@end
+
+@implementation MarkFriendViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

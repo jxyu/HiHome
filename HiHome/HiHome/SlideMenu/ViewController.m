fddb0605af9b5063343c7e10530073c96dfa01c3
Rain <lfqyusw@gmail.com>
2015-11-05 16:20:11 +0800

    update

--- HiHome/HiHome/SlideMenu/ViewController.m
+++ HiHome/HiHome/SlideMenu/ViewController.m
@@ -276,8 +276,6 @@ static const CGFloat menuStartNarrowRatio  = 0.70;
  */
 - (void)handlePan:(UIPanGestureRecognizer *)recognizer {
     
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"setNick" object:nil];
-    
     NSLog(@"get pan self.sta = %d",self.sta);
     if(self.sta == kStateOption)//add by wangjc  设置页面禁止左划
     {

afc248342785f52d0a752cb34c9c36ccdba0c52a
Rain <lfqyusw@gmail.com>
2015-11-07 16:54:46 +0800

    反馈和功能介绍

---
+++ HiHome/HiHome/UiLayer/MainPage/Options/FunctionDescViewController.h
@@ -0,0 +1,13 @@
+//
+//  FunctionDescViewController.h
+//  HiHome
+//
+//  Created by Rain on 15/11/7.
+//  Copyright © 2015年 zykj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FunctionDescViewController : UIViewController
+
+@end
